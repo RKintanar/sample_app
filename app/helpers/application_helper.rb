cbc0f3ef383f1fb69eddb231d4bc76384ff2769d
Ryan Kintanar <Ryan.Kintanar@gmail.com>
2011-12-23 16:22:06 -0700

    Done with chap 4

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,12 @@
 module ApplicationHelper
   
   #return a title on a per-page basis.
-  def title
-    base_title = "Ruby on Rails Tutorial Sample App"
-    if @title.nil?
-      base_title
+  def title #method definition
+    base_title = "Ruby on Rails Tutorial Sample App" #variable assignment
+    if @title.nil? #boolean test on title instance variable for nil-ness
+      base_title #implicit return
     else
-      "#{base_title} | #{@title}"
+      "#{base_title} | #{@title}" #string interpolation
     end
   end
 end
